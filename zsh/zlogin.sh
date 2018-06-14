5e9e1183d5ebc84b6f5ca150c2b7ab971eabca3f
Florian Evers <florian-evers@gmx.de>
2018-06-14 21:33:24 +0200

    Fixed gpg-agent invocation and added return code printing to the prompt

--- zsh/zlogin.sh
+++ zsh/zlogin.sh
@@ -1,7 +1,4 @@
-pgrep -l gpg-agent &>/dev/null
-if [[ "$?" != "0" ]]; then
-    gpg-agent --daemon --enable-ssh-support &>/dev/null
-fi
-
-SSH_AUTH_SOCK=/run/user/$(id -u)/gnupg/S.gpg-agent.ssh
-export SSH_AUTH_SOCK
+GPG_TTY=$(tty)
+export GPG_TTY
+gpgconf --launch gpg-agent >/dev/null 2>&1
+export SSH_AUTH_SOCK=/run/user/$(id -ru)/gnupg/S.gpg-agent.ssh
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -97,7 +97,9 @@ function prompt_char {
 	if [ $UID -eq 0 ]; then echo "#"; else echo $; fi
 }
 
+local return_code="%(?..%{$fg[red]%}%? ↵%{$reset_color%})"
 PROMPT='%(!.%{$fg_bold[red]%}.%{$fg_bold[green]%}%n@)%m %{$fg_bold[blue]%}%(!.%1~.%~) $(git_prompt_info)%_$(prompt_char)%{$reset_color%} '
+RPS1="${return_code}"
 ZSH_THEME_GIT_PROMPT_PREFIX="("
 ZSH_THEME_GIT_PROMPT_SUFFIX=") "
 
